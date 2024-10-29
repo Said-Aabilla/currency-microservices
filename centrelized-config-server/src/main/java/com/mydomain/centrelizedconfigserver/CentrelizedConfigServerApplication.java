b0e9b2ce0fe7ddaacd8883d635016292535bb43d
Aabilla Said <said160aabilla@gmail.com>
2024-10-29 09:54:53 +0100

    setting up config server

---
+++ centrelized-config-server/src/main/java/com/mydomain/centrelizedconfigserver/CentrelizedConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mydomain.centrelizedconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentrelizedConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentrelizedConfigServerApplication.class, args);
+    }
+
+}
---
+++ centrelized-config-server/src/test/java/com/mydomain/centrelizedconfigserver/CentrelizedConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mydomain.centrelizedconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentrelizedConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
